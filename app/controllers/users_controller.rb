30bbef6aa7e04d1a91721d842eac4355828337a7
mnsmsnr <vanillasky0127@yahoo.co.jp>
2021-04-15 23:01:28 +0900

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # ユーザー登録のためメール認証無効化
+      # @user.send_activation_email
+      # flash[:info] = "Please check your email to activate your account."
+      # redirect_to root_url
     else
       render 'new'
     end
