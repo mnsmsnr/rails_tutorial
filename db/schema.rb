f201ec1dbf9826116cd73e7374926b32294e5679
mnsmsnr <vanillasky0127@yahoo.co.jp>
2021-04-22 22:02:17 +0900

    recreated_env

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20190622045807) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "activation_digest"
-    t.boolean "activated", default: false
+    t.boolean "activated", default: true
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
